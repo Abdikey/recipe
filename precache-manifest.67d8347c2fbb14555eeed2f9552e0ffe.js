8c9b166db5fa17997e276e6c42dd2b93a444fd3b
Abdikey <amoulid2015@gmail.com>
2020-01-08 11:47:29 +0300

    Updates

---
+++ precache-manifest.67d8347c2fbb14555eeed2f9552e0ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bcada5c53888f1109b0d2a460da709",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "dfd5c6088e631e4db596",
+    "url": "/recipe/static/css/main.48d7547f.chunk.css"
+  },
+  {
+    "revision": "8c7879b8cca747c10957",
+    "url": "/recipe/static/js/2.6c6d904f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/recipe/static/js/2.6c6d904f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfd5c6088e631e4db596",
+    "url": "/recipe/static/js/main.d87c6002.chunk.js"
+  },
+  {
+    "revision": "9a6731be517318d420be",
+    "url": "/recipe/static/js/runtime-main.804f54ca.js"
+  }
+]);
